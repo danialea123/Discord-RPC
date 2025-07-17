265f2228e2b76a0b4e6a31c671a3948e88339070
danialea123 <danialbagheri1386@gmail.com>
2025-07-17 05:49:10 +0330

    three

--- src/index.js
+++ src/index.js
@@ -128,3 +128,14 @@ process.on('unhandledRejection', (error) => {
 })
 
 validateConfig()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Discord-RPC is alive!')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App is running on port ' + (process.env.PORT || 3000))
+})
